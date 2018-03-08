fde8101b19021ae4c742845b3e333f22249a4f9f
viktharien <viktharien@zoho.com>
2018-03-07 19:47:07 -0600

    generate models for word, phrase, and category

--- db/migrate/20180308014411_create_phrases.rb
+++ db/migrate/20180308014411_create_phrases.rb
@@ -3,7 +3,8 @@ class CreatePhrases < ActiveRecord::Migration[5.1]
     create_table :phrases do |t|
       t.references :category, foreign_key: true
       t.references :word, foreign_key: true
-
+      t.text :english
+      t.text :normansk
       t.timestamps
     end
   end
