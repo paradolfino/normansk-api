1bc0f57308773c30b9c635efeb2bb4ff6efee653
viktharien <viktharien@zoho.com>
2018-03-07 19:51:46 -0600

    implement validations and relationships for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
     has_many :words
     has_many :phrases
-    validates :english, presence: true, length: { minimum: 3}
-    validates :normansk, presence: true, length: { minimum: 3}
+    validates :name, presence: true
 end
