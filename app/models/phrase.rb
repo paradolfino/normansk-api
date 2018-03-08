60e011620d8cf9cdab898e1f7489d00831c37b8f
viktharien <viktharien@zoho.com>
2018-03-07 19:51:35 -0600

    implement validations and relationships for models

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -2,5 +2,5 @@ class Phrase < ApplicationRecord
   belongs_to :category
   belongs_to :word
   validates :english, presence: true, length: { minimum: 3}
-    validates :normansk, presence: true, length: { minimum: 3}
+  validates :normansk, presence: true, length: { minimum: 3}
 end
