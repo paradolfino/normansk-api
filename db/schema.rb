205dd6c679b070ebce422a109872172e99f46e0b
viktharien <viktharien@zoho.com>
2018-03-07 19:47:15 -0600

    generate models for word, phrase, and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308014348) do
+ActiveRecord::Schema.define(version: 20180308014411) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "phrases", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "word_id"
+    t.text "english"
+    t.text "normansk"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_phrases_on_category_id"
+    t.index ["word_id"], name: "index_phrases_on_word_id"
   end
 
   create_table "words", force: :cascade do |t|
