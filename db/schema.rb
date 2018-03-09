8b4bbab291a9971bece0c80dacbcfa4da3f7b058
viktharien <viktharien@zoho.com>
2018-03-08 19:50:15 -0600

    fix migrations for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309014524) do
+ActiveRecord::Schema.define(version: 20180309014925) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "phrases", force: :cascade do |t|
