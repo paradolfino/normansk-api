876433e12eb59e6768e3d59472de9daea8f423f6
viktharien <viktharien@zoho.com>
2018-03-14 19:45:01 -0500

    begin implement of friendly_id

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,8 @@
 class Word < ApplicationRecord
   belongs_to :category
   has_many :phrases,:dependent => :destroy
+  extend FriendlyId
+  friendly_id :name, use: :slugged
   validates :english, presence: true, length: { minimum: 1}, uniqueness: true
   validates :normansk, presence: true, length: { minimum: 1}, uniqueness: true
 end
